c787df42b9de70055307e6080f064bb0b2a42f35
MikeHatchi22 <hatchi.mike@gmail.com>
2024-10-15 11:12:56 +0200

    Initialize project

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const wilder1 = 'Florentin';
+const wilder2 = 'Emma';
+const wilder3 = 'Cédric';
+const wilder4 = 'Mike';
+
+console.log(chalk.blue(wilder1));
+console.log(chalk.red(wilder2));
+console.log(chalk.yellow(wilder3));
+console.log(chalk.green(wilder4));
